4dce4a3cfb1167f76a62fcc1b6134321ef01c2bf
kravninja <tita0@chicagobooth.edu>
2022-10-27 20:19:49 +0000

    Added arrays and hashes

--- app/controllers/currpairs_controller.rb
+++ app/controllers/currpairs_controller.rb
@@ -3,8 +3,9 @@ class CurrpairsController < ApplicationController
 
     @raw_data = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
 
-    #@array_of_symbols 
+    @array_of_symbols = @symbols_hash.keys
     render({:template => "currency_templates/forex.html.erb"})
   end
 end
