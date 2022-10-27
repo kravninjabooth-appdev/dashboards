8c906065e3990788f8b3bba9815352074eb8e818
kravninja <tita0@chicagobooth.edu>
2022-10-27 22:45:19 +0000

    Added from symbol

--- app/controllers/currpairs_controller.rb
+++ app/controllers/currpairs_controller.rb
@@ -10,14 +10,15 @@ class CurrpairsController < ApplicationController
   end
 
   def second_currency
-    #display_cur_conversion   
+    #display_cur_conversion 
+    #Parameters: {"cur_symbol"=>"AED"}  
 
     @raw_data = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch("symbols")
-
     @array_of_symbols = @symbols_hash.keys
-    #@a_conversion = @symbols_hash.keys
+    
+    @from_symbol = params.fetch("cur_symbol")
     render({:template => "currency_templates/second_currency.html.erb"})
   end
 
