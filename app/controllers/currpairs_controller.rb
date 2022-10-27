760103dc085f19644e82b15c134d6dad1e332f0d
kravninja <tita0@chicagobooth.edu>
2022-10-27 20:10:30 +0000

    Added JSON and api link

--- app/controllers/currpairs_controller.rb
+++ app/controllers/currpairs_controller.rb
@@ -1,5 +1,10 @@
 class CurrpairsController < ApplicationController
   def forex   
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+
+    #@array_of_symbols 
     render({:template => "currency_templates/forex.html.erb"})
   end
 end
