497a169d2a860371ee0bb25695ae7b47a6458bd6
kravninja <tita0@chicagobooth.edu>
2022-10-28 01:06:49 +0000

    currency conversion

--- app/controllers/currpairs_controller.rb
+++ app/controllers/currpairs_controller.rb
@@ -26,13 +26,16 @@ class CurrpairsController < ApplicationController
     #display_cur_conversion 
     #Parameters: {"cur_symbol"=>"AED"}  
 
-    @raw_data = open(" https://api.exchangerate.host/convert?from=#{@from
-    }&to=#{}").read
-    @parsed_data = JSON.parse(@raw_data)
-    @symbols_hash = @parsed_data.fetch("symbols")
-    @array_of_symbols = @symbols_hash.keys
-    
     @from_symbol = params.fetch("cur_symbol")
+    @conversion_symbol = params.fetch("cur_conversion")
+
+    @raw_data = open("https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@conversion_symbol}").read
+    @parsed_data = JSON.parse(@raw_data)
+    @info_hash = @parsed_data.fetch("info")
+    @rate_hash = @info_hash.fetch("rate")
+    # @array_of_symbols = @symbols_hash.keys
+    
+    
     render({:template => "currency_templates/currency_conversion.html.erb"})
   end
 
